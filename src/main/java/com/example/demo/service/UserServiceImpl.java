f76cf1a53b61037eedb2a5a22a6232510d0a83e2
bala-amypo <bala@amypo.in>
2025-12-18 16:03:07 +0000

    User commit

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.Role;
 import com.example.demo.entity.User;
 import com.example.demo.repository.RoleRepository;
@@ -13,32 +13,35 @@ import com.example.demo.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired private UserRepository userRepository;
-    @Autowired private RoleRepository roleRepository;
 
-    //private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Override
     public User registerUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        // NO PASSWORD ENCODING FOR NOW
         Role userRole = roleRepository.findByName("USER").orElse(null);
-        if(userRole!=null) user.setRoles(Collections.singleton(userRole));
+        if (userRole != null) {
+            user.setRoles(Collections.singleton(userRole));
+        }
         return userRepository.save(user);
     }
 
     @Override
-    public User findByEmail(String email){
+    public User findByEmail(String email) {
         return userRepository.findByEmail(email).orElse(null);
     }
 
     @Override
-    public User getUserById(Long id){
+    public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
 
     @Override
-public List<User> getAllUsers() {
-    return userRepository.findAll();
-}
-
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
