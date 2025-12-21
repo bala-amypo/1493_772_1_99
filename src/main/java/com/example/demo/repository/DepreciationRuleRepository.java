d1de319a3c17a8270f281be1e4491feee6afeeef
bala-amypo <bala@amypo.in>
2025-12-21 05:15:13 +0000

    User commit

--- src/main/java/com/example/demo/repository/DepreciationRuleRepository.java
+++ src/main/java/com/example/demo/repository/DepreciationRuleRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.DepreciationRule;
+import java.util.Optional;
 
-public interface DepreciationRuleRepository extends JpaRepository<DepreciationRule, Long> {}
+
+public interface DepreciationRuleRepository extends JpaRepository<DepreciationRule, Long> {
+Optional<DepreciationRule> findByRuleName(String ruleName);
+}
--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Vendor;
+import java.util.Optional;
 
-public interface VendorRepository extends JpaRepository<Vendor, Long> {}
+
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+Optional<Vendor> findByVendorName(String vendorName);
+}
