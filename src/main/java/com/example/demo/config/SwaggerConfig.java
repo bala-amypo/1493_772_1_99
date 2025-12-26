26149b91a6e2c2e96e15fb264f82aac213fdd8e0
bala-amypo <bala@amypo.in>
2025-12-26 16:24:58 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,38 +1,18 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
-
-        SecurityScheme bearerScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                // Server URL
-                .servers(List.of(
-                        new Server().url("https://9017.pro604cr.amypo.ai/")
-                ))
-                // JWT Security Definition
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", bearerScheme)
-                )
-                // Apply Security Globally
-                .addSecurityItem(
-                        new SecurityRequirement().addList("bearerAuth")
-                );
+                .info(new Info()
+                        .title("Asset Lifecycle API")
+                        .version("1.0"));
     }
 }
