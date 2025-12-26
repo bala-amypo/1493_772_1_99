e5cf094c53bf1a42048d2e0bf07d53549f65c682
bala-amypo <bala@amypo.in>
2025-12-26 10:27:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/AssetDisposalController.java
+++ src/main/java/com/example/demo/controller/AssetDisposalController.java
@@ -1,34 +1,23 @@
 package com.example.demo.controller;
 
-
-import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.AssetDisposal;
 import com.example.demo.service.AssetDisposalService;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/disposals")
 public class AssetDisposalController {
+    
+    @Autowired private AssetDisposalService disposalService;
 
+    @PostMapping("/request/{assetId}")
+    public AssetDisposal requestDisposal(@PathVariable Long assetId, @RequestBody AssetDisposal disposal) {
+        return disposalService.requestDisposal(assetId, disposal);
+    }
 
-private final AssetDisposalService service;
-
-
-public AssetDisposalController(AssetDisposalService service) {
-this.service = service;
-}
-
-
-@PostMapping("/request/{assetId}")
-public AssetDisposal request(@PathVariable Long assetId,
-@RequestBody AssetDisposal disposal) {
-return service.requestDisposal(assetId, disposal);
-}
-
-
-@PutMapping("/approve/{disposalId}/{adminId}")
-public AssetDisposal approve(@PathVariable Long disposalId,
-@PathVariable Long adminId) {
-return service.approveDisposal(disposalId, adminId);
-}
+    @PutMapping("/approve/{disposalId}/{adminId}")
+    public AssetDisposal approveDisposal(@PathVariable Long disposalId, @PathVariable Long adminId) {
+        return disposalService.approveDisposal(disposalId, adminId);
+    }
 }
