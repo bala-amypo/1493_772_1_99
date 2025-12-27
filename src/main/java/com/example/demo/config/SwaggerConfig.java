80dceea7d886f402e9aa2ed2ff0b25f019578d64
bala-amypo <bala@amypo.in>
2025-12-27 01:42:49 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,18 +1,19 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Asset Lifecycle API")
-                        .version("1.0"));
+                .servers(List.of(
+                        new Server().url("http://9017.pro604cr.amypo.ai/") // must be HTTP
+                ));
     }
 }
